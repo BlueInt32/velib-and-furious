57f06f759a705d37ebe1400b895c539843c42c9d
OussamaAssassi <oussama.assassi@gmail.com>
2014-06-13 17:11:06 +0200

    fullscreen map

--- js/controllers/stationCtrl.js
+++ js/controllers/stationCtrl.js
@@ -7,7 +7,7 @@ angular.module('velibAndFurious')
             $scope.stationList = function () {
 
                 var apiKey = 'e5bc60b81a3a39b0d932e3d7a09cf07184ba8d29';
-                
+                https://api.jcdecaux.com/vls/v1/stations?apiKey=e5bc60b81a3a39b0d932e3d7a09cf07184ba8d29
                 $http.jsonp('https://api.jcdecaux.com/vls/v1/stations?apiKey='+apiKey+'&callback=JSON_CALLBACK').
                     success(function (data) {
                         $scope.stations = data;
